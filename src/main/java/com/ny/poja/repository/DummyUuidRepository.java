88f382761669d937ccdb00dd76cac36fe83cc856
NyH1409 <hei.nyhasina@gmail.com>
2023-11-29 07:40:42 +0300

    feat: init poja project

---
+++ src/main/java/com/ny/poja/repository/DummyUuidRepository.java
@@ -0,0 +1,15 @@
+package com.ny.poja.repository;
+
+import com.ny.poja.PojaGenerated;
+import com.ny.poja.repository.model.DummyUuid;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@PojaGenerated
+@Repository
+public interface DummyUuidRepository extends JpaRepository<DummyUuid, String> {
+
+  @Override
+  Optional<DummyUuid> findById(String id);
+}
---
+++ src/main/java/com/ny/poja/service/event/UuidCreatedService.java
@@ -0,0 +1,26 @@
+package com.ny.poja.service.event;
+
+import com.ny.poja.PojaGenerated;
+import com.ny.poja.endpoint.event.gen.UuidCreated;
+import com.ny.poja.repository.DummyUuidRepository;
+import com.ny.poja.repository.model.DummyUuid;
+import java.util.function.Consumer;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@PojaGenerated
+@Service
+@AllArgsConstructor
+@Slf4j
+public class UuidCreatedService implements Consumer<UuidCreated> {
+
+  private final DummyUuidRepository dummyUuidRepository;
+
+  @Override
+  public void accept(UuidCreated uuidCreated) {
+    var dummyUuid = new DummyUuid();
+    dummyUuid.setId(uuidCreated.getUuid());
+    dummyUuidRepository.save(dummyUuid);
+  }
+}
